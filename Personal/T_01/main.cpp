ae314a3d27cbaf3575710603f2bc02927d215c5e
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-08-25 16:57:43 +0000

    Fri, Aug 25, 2023, 9:57 AM -07:00

--- Personal/T_01/main.cpp
+++ Personal/T_01/main.cpp
@@ -6,7 +6,13 @@ using namespace std;
 
 int main()
 {
-    
+    int array[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = pow(2, i+1);
+        cout << array [i] << endl;
+    }
 
     return 0;
 }
