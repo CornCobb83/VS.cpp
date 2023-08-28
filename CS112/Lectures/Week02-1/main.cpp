84e04b8df0f23c684ab802b89674a5787516c9dd
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-08-28 16:51:37 +0000

    Mon, Aug 28, 2023, 9:51 AM -07:00

--- CS112/Lectures/Week02-1/main.cpp
+++ CS112/Lectures/Week02-1/main.cpp
@@ -3,12 +3,18 @@
 #include <cstdlib>
 #include <string>
 #include <iostream>
+#include "letter_match.h"
 
 using namespace std;
 
 int main()
 {
+    cout << boolalpha;
 
+    cout << "*** TESTING letter_mathch ***" << endl;
 
-    return 0;
+    cout << (letter_match("guess", "grass", 3) returns true) << endl;
+    cout << (letter_match("guess", "grass", 2) returns false) << endl;
+
+    return EXIT_SUCCESS;
 }
