d84e997ef70339998a6e3d96a314c0b12d133b19
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-11 17:57:54 +0000

    Mon, Sep 11, 2023, 10:57 AM -07:00

--- CS112/Assignment01/Part1/change_calc.cpp
+++ CS112/Assignment01/Part1/change_calc.cpp
@@ -12,21 +12,29 @@ void getChange(int& pennies, int& nickles, int& dimes, int& quarters)
 {
     cout << "How many quarters?" << endl;
     cin >> quarters;
+    if (!is_digit(quarters))
+        quarters = 0;
     quarters = quarters * 25;
     //cout << quarters << endl;
 
     cout << "How many dimes?" << endl;
     cin >> dimes;
+    if (!is_digit(dimes))
+        dimes = 0;
     dimes = dimes * 10;
     //cout << dimes << endl;
 
     cout << "How many nickles?" << endl;
     cin >> nickles;
+    if (!is_digit(nickles))
+        nickles = 0;
     nickles = nickles * 5;
     //cout << nickles << endl;
 
     cout << "How many pennies?" << endl;
     cin >> pennies;
+    if (!is_digit(pennies))
+        pennies = 0;
     //cout << pennies << endl;
 
     cout << endl;
