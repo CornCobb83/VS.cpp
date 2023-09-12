e9b60427c7a3838774c51687bbdaca1285594b57
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-12 20:36:00 +0000

    Tue, Sep 12, 2023, 1:36 PM -07:00

--- CS112/Assignments/Asign01/prime_generator.cpp
+++ CS112/Assignments/Asign01/prime_generator.cpp
@@ -15,13 +15,15 @@ void prime_generator()
     cout << "Enter a num\n";
     cin >> num;
 
-    for (int i = 2; i <= num; i++) {
-        if (i == 2 || i == 3 || i == 5 || i == 7)
-            cout << i << " ";
-        else if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0) || (i % 7 == 0))
-            cout << "";
-        else
-            cout << i << " ";
+    if (isdigit(num)) {
+        for (int i = 2; i <= num; i++) {
+            if (i == 2 || i == 3 || i == 5 || i == 7)
+                cout << i << " ";
+            else if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0) || (i % 7 == 0))
+                cout << "";
+            else
+                cout << i << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
 }
