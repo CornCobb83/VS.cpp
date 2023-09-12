18c426fabab7c874653c45fc229063a84d9611d7
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-12 20:43:37 +0000

    Tue, Sep 12, 2023, 1:43 PM -07:00

---
+++ CS112/Assignments/Asign01/rochambeau.h
@@ -0,0 +1,15 @@
+// CS 112 Fall 2023 – Assignment #1
+// Jordan Cobb
+
+#ifndef ROCHAMBEAU_H
+#define ROCHAMBEAU_H
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+double change_calc(int, int, int, int);
+
+#endif
