2b05d85ca401f27758d477d90329d27e1dee0638
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-12 21:00:29 +0000

    Tue, Sep 12, 2023, 2:00 PM -07:00

--- CS112/Assignments/Asign01/rochambeau.cpp
+++ CS112/Assignments/Asign01/rochambeau.cpp
@@ -12,5 +12,10 @@ using namespace std;
 
 void rochambeau()
 {
+    srand(time(NULL));
+    int num = (rand() % 3) + 1;
+    char input;
 
+    cout << "Enter 'r' (rock), 'p' (paper), 's' (scissors)";
+    cin >> 
 }
