e96c7e69a8c06fda7e9f918271e6d33cedb59f0d
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-13 16:44:25 +0000

    Wed, Sep 13, 2023, 9:44 AM -07:00

--- Personal/T_01/main.cpp
+++ Personal/T_01/main.cpp
@@ -2,26 +2,9 @@
 using namespace std;
 
 int main() {
-   int stop;
-   int result;
+
    int a;
-   int b;
-
-   cin >> stop;
-   result = 0;
-
-   for (a = 0; a < 4; ++a) {
-      cout << a << ": ";
-      for (b = 0; b < 3; ++b) {
-         result += a + b;
-         if (result > stop) {
-            cout << "_ ";
-            continue;
-         }
-         cout << result << ",";
-      }
-      cout << endl;
-   }
+   cout << a << endl;
 
    return 0;
 }
