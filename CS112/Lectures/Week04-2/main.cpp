e57b4c943df373a2d28e9d610f40b1176eee5a1a
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-13 16:16:34 +0000

    Wed, Sep 13, 2023, 9:16 AM -07:00

--- CS112/Lectures/Week04-2/main.cpp
+++ CS112/Lectures/Week04-2/main.cpp
@@ -8,7 +8,14 @@ int main()
 {
     cout << boolalpha;
 
-    new int
+    int *intPtr;
+
+    intPtr = new int;
+
+    *intPtr = 75;
+    cout << *intPtr << endl;
+
+    
 
     return EXIT_SUCCESS;
 }
