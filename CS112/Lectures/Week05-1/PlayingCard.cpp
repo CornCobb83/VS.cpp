183465fd9a9cd6ce4735d5eeada3fb5eac683012
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-18 16:44:26 +0000

    Mon, Sep 18, 2023, 9:44 AM -07:00

--- CS112/Lectures/Week05-1/PlayingCard.cpp
+++ CS112/Lectures/Week05-1/PlayingCard.cpp
@@ -7,3 +7,22 @@ using namespace std;
 
 PlayigCard::PlayingCard() {
 }
+
+PlayingCard::PlayingCard(int a_rank, char a_suit) {
+    if ((a_rank < 1) || (a_rank > 13)) {
+        cout << "*** ILLEGAL RANK - SETTING TO 0 ***" << endl;
+        rank = 0;
+    }
+    else {
+        rank = a_rank;
+    }
+    if (suit != 'H' && suit != 'D' && suit != 'S' && suit != 'C') {
+        cout << "*** ILLEGAL SUIT - SETTING TO X ***" << endl;
+        suit = 'X';
+    }
+    else {
+        suit = a_suit;
+    }
+    
+    viaible = false
+}
