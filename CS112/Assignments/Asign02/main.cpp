31c7231e4c45215d00e23ec8a8b5f3d91e9679b3
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-21 20:41:48 +0000

    Thu, Sep 21, 2023, 1:41 PM -07:00

--- CS112/Assignments/Asign02/main.cpp
+++ CS112/Assignments/Asign02/main.cpp
@@ -19,6 +19,8 @@ int main() {
     bool cedricActive = true;
     bool eloiseActive = true;
 
+    int win1 = 0, win2 = 0, win3 = 0;
+
     cout << "How many duels would you like to create?\n";
     int count;
     cin >> count;
@@ -26,11 +28,19 @@ int main() {
     if (count > 0) {
         for (int i = 0; i < count; i++) {
             int duel = startDuel(angelaActive, cedricActive, eloiseActive, ANGELINA_WANDPOWER, CEDRIC_WANDPOWER, ELOISE_WANDPOWER);
-            cout << duel << endl;
+            cout << startDuel(angelaActive, cedricActive, eloiseActive, ANGELINA_WANDPOWER, CEDRIC_WANDPOWER, ELOISE_WANDPOWER) << endl;
+            if (duel == 1)
+                win1++;
+            else if (duel == 2)
+                win2++;
+            else
+                win3++;
         }
     }
     else
         cout << "Invalid input\n";
+
+    cout << win1 << "%   " << win2 << "%   " << win3 << "%\n";
 
     //spell(eloiseActive, ELOISE_WANDPOWER);
 
