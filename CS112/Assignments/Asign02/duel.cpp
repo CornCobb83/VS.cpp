422cba47d89ffd6d75f5bf2aebfee8a71f0fb98e
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-21 18:54:35 +0000

    Thu, Sep 21, 2023, 11:54 AM -07:00

--- CS112/Assignments/Asign02/duel.cpp
+++ CS112/Assignments/Asign02/duel.cpp
@@ -12,9 +12,14 @@ using namespace std;
 void spell(bool& opponentActive, double wandPower)
 {
     if (opponentActive) {
+        for (int i = 0; i < 10; i++) {
         int roll = (rand() % 4) + 1;
         cout << roll << endl;
-        if ()
+        if (roll <= (wandPower * 4))
+            cout << "hit" << endl;
+        else
+            cout << "miss" <<  endl;
+        }
     }
     else
         cout << "Casted on an unconcious opponent\n";
