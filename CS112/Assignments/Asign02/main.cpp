8c104112d5b16f191c318662558d7b25f9894492
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-27 20:14:23 +0000

    Wed, Sep 27, 2023, 1:14 PM -07:00

--- CS112/Assignments/Asign02/main.cpp
+++ CS112/Assignments/Asign02/main.cpp
@@ -22,36 +22,36 @@ int main() {
 
     double win1 = 0.00, win2 = 0.00, win3 = 0.00;
 
-    cout << "How many duels would you like to create?\n";
-    int count;
-    cin >> count;
-    cout << endl;
-    if (count > 0) {
-        for (int i = 0; i < count; i++) {
-            int duel = startDuel();
-            cout << duel << endl;
+    // cout << "How many duels would you like to create?\n";
+    // int count;
+    // cin >> count;
+    // cout << endl;
+    // if (count > 0) {
+    //     for (int i = 0; i < count; i++) {
+    //         int duel = startDuel();
+    //         cout << duel << endl;
 
-            // For me to evaluate win %s
-            if (duel == 1)
-                win1++;
-            else if (duel == 2)
-                win2++;
-            else
-                win3++;
-        }
-    }
-    else
-        cout << "Invalid input\n";
+    //         // For me to evaluate win %s
+    //         if (duel == 1)
+    //             win1++;
+    //         else if (duel == 2)
+    //             win2++;
+    //         else
+    //             win3++;
+    //     }
+    // }
+    // else
+    //     cout << "Invalid input\n";
 
-    double win4 = (win1 / (count + 0.00)) * 100;
-    double win5 = (win2 / (count + 0.00)) * 100;
-    double win6 = (win3 / (count + 0.00)) * 100;
+    // double win4 = (win1 / (count + 0.00)) * 100;
+    // double win5 = (win2 / (count + 0.00)) * 100;
+    // double win6 = (win3 / (count + 0.00)) * 100;
 
-    cout << fixed << setprecision(0) << "Angela: " << win1 << " wins - " << win4 << "%   "
-                                    << "Cedric: " << win2 << " wins - " << win5 << "%   "
-                                    << "Eloise: " << win3 << " wins - " << win6 << "%\n";
+    // cout << fixed << setprecision(0) << "Angela: " << win1 << " wins - " << win4 << "%   "
+    //                                 << "Cedric: " << win2 << " wins - " << win5 << "%   "
+    //                                 << "Eloise: " << win3 << " wins - " << win6 << "%\n";
 
-    //spell(eloiseActive, ELOISE_WANDPOWER);
+    spell(eloiseActive, ELOISE_WANDPOWER);
 
     return 0;
 }
