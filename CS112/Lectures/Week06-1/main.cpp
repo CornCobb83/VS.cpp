44c2d180a2b883caa5f43db8a1e65e81e52e41bd
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-09-27 16:58:08 +0000

    Wed, Sep 27, 2023, 9:58 AM -07:00

--- CS112/Lectures/Week06-1/main.cpp
+++ CS112/Lectures/Week06-1/main.cpp
@@ -9,9 +9,41 @@ using namespace std;
 int main() {
     cout << boolalpha;
 
+    cout << fixed << setprecision(2);
+
+    //Test constructors
     GraphPoint myPoint;
-    cout  << "Zero arg myPoint: ";
+    cout << "Zero arg myPoint:";
     myPoint.display();
+
+    GraphPoint otherPoint(3.0, 4.0);
+    cout << "two arg otherPoint: ";
+    otherPoint.display();
+
+    cout << "X coord of other Point is "
+        << otherPoint.getX() << endl;
+    cout << "Y coord of other Point is "
+        << otherPoint.getY() << endl;
+
+    cout << "otherPoint changed to : ";
+    otherPoint.setX(27.4567);
+    otherPoint.setY(-456.6725);
+    otherPoint.display();
+
+    cout << "DIstance: ";
+    cout << myPoint.distance(otherPoint) << endl;
+
+    cout << "Assign newPoint to be other:";
+    GraphPoint = newPoint();
+    newPoint = otherPoint;
+    newPoint.display();
+
+    cout << "Are otherPoint and newPoint the same? ";
+    cout << (newPoint == otherPoint) << endl;
+
+    cout << "Changing newPoint to: ";
+    newPoint
+    newPoint.display();
 
     return EXIT_SUCCESS;
 }
