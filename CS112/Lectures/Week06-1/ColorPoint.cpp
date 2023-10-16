29eca626a9c8453e77583987dc1f5017c351624b
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-10-16 16:39:06 +0000

    Mon, Oct 16, 2023, 9:39 AM -07:00

--- CS112/Lectures/Week06-1/ColorPoint.cpp
+++ CS112/Lectures/Week06-1/ColorPoint.cpp
@@ -6,3 +6,15 @@
 #include <cmath>
 
 using namespace std;
+
+ColorPoint::ColorPoint() : GraphPoint() {
+    colorValue = COLOR_DEFAULT;
+}
+
+ColorPoint::ColorPoint(double x, double y) : GraphPoint(x, y) {
+    colorValue = COLOR_DEFAULT;
+}
+
+ColorPoint::ColorPoint(double x, double y, PointColor color) : GraphPoint(x, y) {
+    colorValue = color;
+}
