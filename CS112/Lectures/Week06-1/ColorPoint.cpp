f942b8b75733cbfc2e3041caed21e42d3a68729c
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-10-18 16:14:25 +0000

    Wed, Oct 18, 2023, 9:14 AM -07:00

--- CS112/Lectures/Week06-1/ColorPoint.cpp
+++ CS112/Lectures/Week06-1/ColorPoint.cpp
@@ -1,26 +1,29 @@
+// CS 112 Fall 2023 - Week 09 Lecture 1
+// C++ Derived Classes
 #include <cstdlib>
-#include <iostream>
-#include <iomanip>
 #include <string>
-#include "ColorPoint.h"
+#include <iostream>
 #include <cmath>
+// #include "GraphPoint.h"
+#include "ColorPoint.h"
 
 using namespace std;
-
-ColorPoint::ColorPoint() : GraphPoint() {
-    colorValue = COLOR_DEFAULT;
+// Constructors
+ColorPoint::ColorPoint(): GraphPoint() {
+colorValue = COLOR_DEFAULT;
 }
-
-ColorPoint::ColorPoint(double x, double y) : GraphPoint(x, y) {
-    colorValue = COLOR_DEFAULT;
+ColorPoint::ColorPoint(double x, double y): GraphPoint(x, y) {
+colorValue = COLOR_DEFAULT;
 }
-
-ColorPoint::ColorPoint(double x, double y, PointColor color) : GraphPoint(x, y) {
-    colorValue = color;
+ColorPoint::ColorPoint(double x, double y, PointColor color):
+GraphPoint(x, y) {
+colorValue = color;
 }
-
+// ACCESSORS
+// MUTATORS
+// OTHER METHODS
 void ColorPoint::display() const {
-    cout << "ColorPoint is " << colorValue << " at "
-         << "(" << xCoord << ", "
-         << yCoord << ")" << endl;
+cout << "ColorPoint is " << COLORNAMES[colorValue] << " at "
+<< "(" << this->getX() << ", "
+<< this->getY() << ")" << endl;
 }
