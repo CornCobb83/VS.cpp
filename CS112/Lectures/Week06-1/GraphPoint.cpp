b73a0a4de8edba8408dba5deebedc90415fbf131
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-10-18 16:57:02 +0000

    Wed, Oct 18, 2023, 9:57 AM -07:00

--- CS112/Lectures/Week06-1/GraphPoint.cpp
+++ CS112/Lectures/Week06-1/GraphPoint.cpp
@@ -26,84 +26,84 @@ using namespace std;
 // The initialization section of a constructor can be used to
 // set the initial values of data members
 
-    GraphPoint::GraphPoint() : xCoord(X_DEFAULT), yCoord(Y_DEFAULT) {
-    }
+GraphPoint::GraphPoint() : xCoord(X_DEFAULT), yCoord(Y_DEFAULT) {
+}
 
-    GraphPoint::GraphPoint(double x, double y) {
-        xCoord = x;
-        yCoord = y;
-    }
+GraphPoint::GraphPoint(double x, double y) {
+    xCoord = x;
+    yCoord = y;
+}
 
-    GraphPoint::GraphPoint(double r, int theta) {
-        int thetaNormalized = (theta + 36000) % 360;
-        double thetaRadians = (thetaNormalized * M_PI) / 180.0;
-        xCoord = r * cos(thetaRadians);
-        yCoord = r * sin(thetaRadians);
-    }
+GraphPoint::GraphPoint(double r, int theta) {
+    int thetaNormalized = (theta + 36000) % 360;
+    double thetaRadians = (thetaNormalized * M_PI) / 180.0;
+    xCoord = r * cos(thetaRadians);
+    yCoord = r * sin(thetaRadians);
+}
 // ACCESSORS
 
-    double GraphPoint::getX() const {
-        return xCoord;
-    }
+double GraphPoint::getX() const {
+    return xCoord;
+}
 
-    double GraphPoint::getY() const {
-        return yCoord;
-    }
+double GraphPoint::getY() const {
+    return yCoord;
+}
 
 // MUTATORS
-    void GraphPoint::setX(double newX) {
-        xCoord = newX;
-    }
+void GraphPoint::setX(double newX) {
+    xCoord = newX;
+}
 
-    void GraphPoint::setY(double newY) {
-        yCoord = newY;
-    }
+void GraphPoint::setY(double newY) {
+    yCoord = newY;
+}
 
-    void GraphPoint::flipCoords() {
-        double temp;
-        temp = xCoord;
-        xCoord = yCoord;
-        yCoord = temp;
-    }
+void GraphPoint::flipCoords() {
+    double temp;
+    temp = xCoord;
+    xCoord = yCoord;
+    yCoord = temp;
+}
 
-    void GraphPoint::resetToOrigin() {
-        xCoord = 0.0;
-        yCoord = 0.0;
-    }
+void GraphPoint::resetToOrigin() {
+    xCoord = 0.0;
+    yCoord = 0.0;
+}
 
-    void GraphPoint::setCoords(double x, double y) {
-        xCoord = x;
-        yCoord = y;
-    }
+void GraphPoint::setCoords(double x, double y) {
+    xCoord = x;
+    yCoord = y;
+}
 
 // OTHER METHODS
-    void GraphPoint::display() const {
-        cout << "Point "
-        << "(" << xCoord << ", "
-        << yCoord << ")" << endl;
-    }
+void GraphPoint::display() const {
+    cout << "Point "
+    << "(" << xCoord << ", "
+    << yCoord << ")" << endl;
+}
 
-    string GraphPoint::to_string() const {
-        return std::to_string(xCoord) + "," + std::to_string(yCoord);
-    }
+string GraphPoint::to_string() const {
+    return std::to_string(xCoord) + "," + std::to_string(yCoord);
+}
 
-    double GraphPoint::distance(const GraphPoint& p2) const {
-        double xDiff = xCoord - p2.getX();
-        double yDiff = yCoord - p2.getY();
-        double xDiffSqr = pow(xDiff, 2.0);
-        double yDiffSqr = pow(yDiff, 2.0);
-        return sqrt(xDiffSqr + yDiffSqr);
-    }
+double GraphPoint::distance(const GraphPoint& p2) const {
+    double xDiff = xCoord - p2.getX();
+    double yDiff = yCoord - p2.getY();
+    double xDiffSqr = pow(xDiff, 2.0);
+    double yDiffSqr = pow(yDiff, 2.0);
+    return sqrt(xDiffSqr + yDiffSqr);
+}
 
-    bool GraphPoint::operator ==(const GraphPoint& rhs) const {
+bool GraphPoint::operator ==(const GraphPoint& rhs) const {
     bool xEqual = (abs(xCoord - rhs.xCoord) < 0.001);
     bool yEqual = (abs(yCoord - rhs.yCoord) < 0.001);
     return (xEqual && yEqual);
-    }
+}
 
-    GraphPoint GraphPoint::operator +(const GraphPoint& rhs) {
-        GraphPoint result;
-        result.setX(xCoord + rhs.getX());
-        result.setY(yCoord + rhs.getY());
-        return result;
-    }
+GraphPoint GraphPoint::operator +(const GraphPoint& rhs) {
+    GraphPoint result;
+    result.setX(xCoord + rhs.getX());
+    result.setY(yCoord + rhs.getY());
+    return result;
+}
