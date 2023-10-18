4c62b551717005ce9a81be26c3100e14af776880
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-10-18 16:15:11 +0000

    Wed, Oct 18, 2023, 9:15 AM -07:00

--- CS112/Lectures/Week06-1/GraphPoint.h
+++ CS112/Lectures/Week06-1/GraphPoint.h
@@ -1,71 +1,50 @@
 #ifndef GRAPHPOINT_H
 #define GRAPHPOINT_H
-
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <cmath>
-#include "ColorPoint.h"
-
 using namespace std;
 
-// USER-DEFINED DATA TYPES
-
-//enum PointColor {Black, Blue, Green, Red, Yellow};
-
+// NAMED CONSTANTS USED IN THE C++ CLASS
 const double X_DEFAULT = 0.0;
 const double Y_DEFAULT = 0.0;
-const string NAME_DEFAULT = "Unnamed";
-//const PointColor COLOR_DEFAULT = Black;
-//const string COLOR_NAMES[5] = {"Black", "Blue", "Green", "Red", "Yellow"};
-
 class GraphPoint {
 public:
+// CONSTRUCTORS
+GraphPoint();
+GraphPoint(double x, double y);
+GraphPoint(double r, int theta);
+// ACCESSORS
+double getX() const;
+double getY() const;
+// MUTATORS
+void setX(double newX);
+void setY(double newY);
+void flipCoords();
+void resetToOrigin();
+void setCoords(double newX, double newY);
+// OTHER METHODS
+void display() const;
+string to_string() const;
+double distance(const GraphPoint& p2) const;
+// OPERATOR METHODS
+bool operator ==(const GraphPoint& rhs) const;
+GraphPoint operator +(const GraphPoint& rhs);
+// protected is a section within the class definition
+// that works like private, but an important difference
+// is that data members and methods listed in the
+// protected section are directly accessible by any
+// derived classes that use this as a base class
+//
+// Since we have methods to get the data members, we'll
+// opt to keep this section private. To use it as a
+// protected section, comment out private and uncomment
+// protected
 
-    GraphPoint();
-
-    GraphPoint(double x, double y);
-
-    GraphPoint(double x, double y, string name);
-
-    GraphPoint(double x, double y, string name, PointColor color);
-
-    GraphPoint(const GraphPoint& existingPoint);
-
-    ~GraphPoint();
-
-    double getX() const;
-    double getY() const;
-    string getName() const;
-    PointColor getColor() const;
-
-    void setX(double newX);
-    void setY(double newY);
-    void setName(string newName);
-    void setColor(PointColor newColor);
-
-    void setCoords(double newX, double newY);
-
-    void display() const;
-
-    string to_string() const;
-
-    void flipPoint();
-
-    double distance(const GraphPoint& p2) const;
-
-    bool operator ==(const GraphPoint& rhs) const;
-    GraphPoint& operator  =(const GraphPoint& rhs);
-    GraphPoint operator +(const GraphPoint& rhs);
-
-//private:
-protected:
-
-    double xCoord;
-    double yCoord;
-    string *pointNamePtr;
-    PointColor colorValue;
-
+private:
+// protected:
+double xCoord;
+double yCoord;
 };
-
-#endif
+#endif /* GRAPHPOINT_H */
