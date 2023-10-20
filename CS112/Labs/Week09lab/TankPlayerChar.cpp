721aed8e10c7315032b2056f45e25596331590fe
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-10-20 16:10:51 +0000

    Fri, Oct 20, 2023, 9:10 AM -07:00

---
+++ CS112/Labs/Week09lab/TankPlayerChar.cpp
@@ -0,0 +1,28 @@
+//    CS 112 Fall 2023 - Week 09 Lab
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "TankPlayerChar.h"
+
+using namespace std;
+
+// CONSTRUCTORS
+
+
+
+// ACCESSORS
+
+
+
+// MUTATORS
+
+
+
+// OPERATOR METHODS
+
+
+
+// OTHER METHODS
+
---
+++ CS112/Labs/Week09lab/TankPlayerChar.h
@@ -0,0 +1,19 @@
+//    CS 112 Fall 2023 - Week 09 Lab
+
+#ifndef TANKPLAYERCHAR_H
+#define TANKPLAYERCHAR_H
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "PlayerChar.h"
+
+using namespace std;
+
+// Definition of derived class TankPlayerChar
+
+
+
+#endif /* TANKPLAYERCHAR_H */
+
