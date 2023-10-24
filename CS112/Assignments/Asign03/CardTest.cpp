80f37cfb7f65df1fed6e2d29d78a2757a0c342bd
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-10-24 04:58:32 +0000

    Mon, Oct 23, 2023, 9:58 PM -07:00

--- CS112/Assignments/Asign03/CardTest.cpp
+++ CS112/Assignments/Asign03/CardTest.cpp
@@ -19,21 +19,26 @@ int main() {
     GameCard card1("Squirtle", 5, "Water");
     GameCard card2;
 
+    cout << "Now we are testing card1 which should be set to 'Squirtle', 5, 'Water'" << endl;
     card1.getName();
     cout << "Is card1 name Squirtle? " << (card1.getName() == "Squirtle") << endl;
     card1.getLevel();
+    cout << "Is card1 level 5? " << (card1.getLevel() == 5) << endl;
     card1.getType();
+    cout << "Is card1 type Water? " << (card1.getName() == "Water") << endl;
 
+    cout << "Now we are changing the values of card2" << endl;
     card2.setName("Charmander");
     card2.setLevel(3);
     card2.setType("Fire");
 
+    cout << "Now we are testing card1 which should be set to 'Charmander', 3, 'Fire'" << endl;
     card2.getName();
     cout << "Is card2 name Charmander? " << (card2.getName() == "Charmander") << endl;
     card2.getLevel();
     cout << "Is card2 level 3? " << (card2.getLevel() == 3) << endl;
     card2.getType();
-    cout << "Is card2 name Charmander? " << (card2.getName() == "Charmander") << endl;
+    cout << "Is card2 type Fire? " << (card2.getName() == "Fire") << endl;
 
     myCardPtr->display();
     card1.display();
