9315de049ca37eee550617cc710e7ba2c43c1f6f
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-10-25 16:10:38 +0000

    Wed, Oct 25, 2023, 9:10 AM -07:00

---
+++ CS112/Lectures/Week06-1/ColorVectorPoint.cpp
@@ -0,0 +1,10 @@
+// CS 112 Fall 2023 - Week 09 Lecture 1
+// C++ Derived Classes
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <cmath>
+// #include "GraphPoint.h"
+#include "ColorVectorPoint.h"
+
+using namespace std;
---
+++ CS112/Lectures/Week06-1/ColorVectorPoint.h
@@ -0,0 +1,22 @@
+// CS 112 Fall 2023 - Week 09 Lecture 1
+// C++ Derived Classes
+#ifndef COLORPOINT_H
+#define COLORPOINT_H
+
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include "ColorVectorPoint.h"
+#include "ColorVectorPoint.h"
+#include "VectorPoint.h"
+using namespace std;
+
+class ColorVectorPoint : public ColorPoint, public VectorPoint {
+public:
+
+    ColorVectorPoint();
+    ColorVectorPoint(double x, double y);
+
+}
+
+#endif
