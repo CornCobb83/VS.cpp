44da34d1c985ceb4852bffac2350506f866f5f1b
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-10-31 01:49:13 +0000

    Mon, Oct 30, 2023, 6:49 PM -07:00

--- CS112/Assignments/Asign04/DerivedCardTest.cpp
+++ CS112/Assignments/Asign04/DerivedCardTest.cpp
@@ -61,9 +61,9 @@ int main() {
     // cout << (card1 == card2) << endl;
 
     SecretRare *myCardPtr;
-    myCardPtr = new GameCard("Pikachu", 7, "Electric");
+    myCardPtr = new GameCard("Pikachu", 7, "Electric", "Ultra Rare", true);
 
-    SecretRare card1("Squirtle", 5, "Water");
+    SecretRare card1("Squirtle", 5, "Water", "common");
     SecretRare card2;
 
     return 0;
