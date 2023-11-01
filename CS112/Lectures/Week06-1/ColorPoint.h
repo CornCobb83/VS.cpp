e288556ed1fb9277385442948bc7ec07c63774bf
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-01 16:17:50 +0000

    Wed, Nov 1, 2023, 9:17 AM -07:00

--- CS112/Lectures/Week06-1/ColorPoint.h
+++ CS112/Lectures/Week06-1/ColorPoint.h
@@ -1,5 +1,6 @@
-// CS 112 Fall 2023 - Week 09 Lecture 1
-// C++ Derived Classes
+// CS 112 Fall 2023 Week 10-2 Lecture
+// Demonstration of C++ Multiple Inheritance
+
 #ifndef COLORPOINT_H
 #define COLORPOINT_H
 
@@ -7,39 +8,48 @@
 #include <string>
 #include <cmath>
 #include "GraphPoint.h"
+
 using namespace std;
 
 // Data Type Definitions
 enum PointColor {Black, Blue, Green, Red, Yellow};
 
-// Constant Definitions
+// Constant definition
 const PointColor COLOR_DEFAULT = Black;
-const string COLORNAMES[5] = {"black", "blue", "green", "red", "yellow"};
+const string COLORNAMES[5] =
+    {"black", "blue", "green", "red", "yellow"};
 
 class ColorPoint : public GraphPoint {
-
-    public:
-
+public:
     ColorPoint();
     ColorPoint(double x, double y);
     ColorPoint(double x, double y, PointColor color);
+    ColorPoint(const ColorPoint& existingPoint);
     ColorPoint(const GraphPoint& existingPoint);
 
     // ACCESSORS
     // Will already inherit getX and getY
+
     PointColor getColor() const;
 
     // MUTATORS
     // Will already inherit setX and setY
+
     void setColor(PointColor newColor);
 
     // OTHER METHODS
+
+    // It's good style to put "virtual" in the method
+    // header here, but it's NOT required!
+
     virtual void display() const;
     string to_string() const;
 
-    private:
+private:
 
     PointColor colorValue;
+
+
 
 };
 
