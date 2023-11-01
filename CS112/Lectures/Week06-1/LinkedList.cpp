292dcf540c7a402efdb06ebb3f1f2f7383e660d8
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-01 16:29:06 +0000

    Wed, Nov 1, 2023, 9:29 AM -07:00

---
+++ CS112/Lectures/Week06-1/LinkedList.cpp
@@ -0,0 +1,25 @@
+// CS 112 Fall 2023 Week 11-1 Lecture
+// Demonstration of C++ Collection Class
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "GraphPoint.h"
+#include "Node.h"
+#include "LinkedList.h"
+
+using namespace std;
+
+// Constructors
+
+LinkedList::LinkedList() {
+    headPtr = NULL;
+}
+
+// Mutators
+
+LinkedList::addToList(double x, double y) {
+    GraphPoint newPoint(x, y);
+    Node
+}
---
+++ CS112/Lectures/Week06-1/LinkedList.h
@@ -0,0 +1,26 @@
+// CS 112 Fall 2023 Week 11-1 Lecture
+// Demonstration of C++ Collection Class
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "LinkedList.h"
+
+using namespace std;
+
+class LinkedList {
+
+    // Constructor
+    LinkedList();
+
+    // Mutators
+    void addToList(double x, double y);
+
+protected:
+
+private:
+
+    Node *headPtr;
+
+};
