afdd3964203df056017fb252773df134ff857606
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-03 18:22:21 +0000

    Fri, Nov 3, 2023, 11:22 AM -07:00

--- CS112/Labs/Week11lab/LinkedList.cpp
+++ CS112/Labs/Week11lab/LinkedList.cpp
@@ -109,7 +109,7 @@ int LinkedList::getValueAt(int nodePosition) const {
 }
 
 bool LinkedList::addNodeAfter(int nodePosition, int newValue) {
-    if (nodePosition < 0 || nodePosition <= listLength())
+    if (nodePosition + 1 > listLength())
         return false;
 
     Node *currentNodePtr = headPtr;
