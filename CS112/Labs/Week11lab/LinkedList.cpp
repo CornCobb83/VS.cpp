05043bf1c23fdcc6f87ce402e9319bdf6eea3541
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-03 17:51:48 +0000

    Fri, Nov 3, 2023, 10:51 AM -07:00

--- CS112/Labs/Week11lab/LinkedList.cpp
+++ CS112/Labs/Week11lab/LinkedList.cpp
@@ -116,7 +116,7 @@ bool LinkedList::addNodeAfter(int nodePosition, int newValue) {
     int length = 0;
 
     while (currentNodePtr != NULL) {
-        if (length == nodePosition) {
+        if (length == nodePosition - 1) {
             Node *newNode = new Node(newValue);
             newNode->setNextPtr(currentNodePtr->getNextPtr());
             currentNodePtr->setNextPtr(newNode);
