34627278983c0f78c1eca2877d2aa6894a50b8b0
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-03 18:58:43 +0000

    Fri, Nov 3, 2023, 11:58 AM -07:00

--- CS112/Labs/Week11lab/LinkedList.cpp
+++ CS112/Labs/Week11lab/LinkedList.cpp
@@ -118,7 +118,7 @@ bool LinkedList::addNodeAfter(int nodePosition, int newValue) {
     }
 
     Node *currentNodePtr = headPtr;
-    int length = 0;
+    int length = 1;
 
     while (currentNodePtr != NULL) {
         if (length == nodePosition - 1) {
@@ -146,7 +146,7 @@ bool LinkedList::removeNodeAt(int nodePosition) {
     }
 
     Node *currentNodePtr = headPtr;
-    int length = 0;
+    int length = 1;
 
     while (currentNodePtr != NULL) {
         if (length == nodePosition) {
