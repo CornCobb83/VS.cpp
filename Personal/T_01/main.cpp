31b1729d10f9608820f2bad37049771e374ae9c7
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-07 21:50:38 +0000

    Tue, Nov 7, 2023, 1:50 PM -08:00

--- Personal/T_01/main.cpp
+++ Personal/T_01/main.cpp
@@ -6,13 +6,17 @@ int main() {
 
    string food = "Pizza";
    string *ptr = &food;
+   cout << endl;
 
    cout << ptr << endl;
+   cout << &ptr << endl;
    cout << *ptr << endl;
 
    food = "Burger";
+   cout << endl;
 
    cout << ptr << endl;
+   cout << &ptr << endl;
    cout << *ptr << endl;
 
    return 0;
