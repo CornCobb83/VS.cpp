188fcf4711ce7aca109faee49017838647cfdfab
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-08 04:34:06 +0000

    Tue, Nov 7, 2023, 8:34 PM -08:00

--- Personal/T_01/main.cpp
+++ Personal/T_01/main.cpp
@@ -19,9 +19,18 @@ int main() {
    cout << &ptr << endl;
    cout << *ptr << endl << endl;
 
-   int array[5] 
+   int *ptr2;
+   int array[5] = {1, 2, 3, 4, 5};
+   ptr2 = &array[0];
+
+   cout << ptr2 << endl;
+   cout << &ptr2 << endl;
+   cout << *ptr2 << endl << endl;
+
+   cout << *(ptr2 + 1) << endl << endl;
 
    delete ptr;
+   delete ptr2;
 
    return 0;
 }
