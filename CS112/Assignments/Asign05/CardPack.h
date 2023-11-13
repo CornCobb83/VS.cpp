985dfc5f4656779bb55faec8cf7ba5a54dc1f146
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-13 19:56:01 +0000

    Mon, Nov 13, 2023, 11:56 AM -08:00

---
+++ CS112/Assignments/Asign05/CardPack.h
@@ -0,0 +1,29 @@
+// Assignment #5
+// Jordan Cobb
+
+#ifndef CARDPACK_H
+#define CARDPACK_H
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+class CardPack {
+public:
+    // Declarations for constructors
+
+    // Declarations for accessors
+
+    // Declarations for mutators
+
+    // Declarations for other methods
+
+private:
+    // Declarations of data members
+
+};
+
+#endif
