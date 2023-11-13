38598f88bf60a0f05d29d85caf9d04c47368d2be
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-13 22:47:37 +0000

    Mon, Nov 13, 2023, 2:47 PM -08:00

---
+++ CS112/Assignments/Asign05/cardTest.cpp
@@ -0,0 +1,21 @@
+// // Assignment #5
+// // Jordan Cobb
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include "GameCard.h"
+#include "SecretRare.h"
+#include "CardPack"
+
+using namespace std;
+
+int main() {
+    cout << boolalpha;
+
+    
+
+    return 0;
+}
