efcfb3e26e8b21ac4f957e4f663fcb9ccbb19085
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-13 17:15:07 +0000

    Mon, Nov 13, 2023, 9:15 AM -08:00

--- Personal/T_01/main.cpp
+++ Personal/T_01/main.cpp
@@ -1,41 +1,32 @@
+#include <string>
 #include <iostream>
 
 using namespace std;
 
 int main() {
+    string inputName;
+    int age;
 
-   string food = "Pizza";
-   string *ptr;
-   ptr = &food;
-   cout << endl;
+    cin.exceptions(ios::failbit);
 
-   cout << ptr << endl;
-   cout << &ptr << endl;
-   cout << *ptr << endl << endl;
+    while (true) {
+        cin >> inputName;
+        if (inputName == "-1") {
+            break;
+        }
 
-   food = "Burger";
+        try {
+            if (!(cin >> age)) {
+                throw inputName;
+            } else {
+                    cout << inputName << " " << (age + 1) << endl;
+                }
+            }
+         catch (string name) {
+            cout << name << " 0" << endl;
+            cin.clear();
+        }
+    }
 
-   cout << ptr << endl;
-   cout << &ptr << endl;
-   cout << *ptr << endl << endl;
-
-   int *ptr2;
-   int array[5] = {1, 2, 3, 4, 5};
-   ptr2 = &array[0];
-
-   cout << ptr2 << endl;
-   cout << &ptr2 << endl;
-   cout << *ptr2 << endl << endl;
-
-   cout << *(ptr2 + 1) << endl << endl;
-
-   int *my_array = new int[10];
-
-   cout << *(my_array + 2) << endl << endl;
-
-   delete ptr;
-   delete ptr2;
-   delete[] my_array;
-
-   return 0;
+    return 0;
 }
