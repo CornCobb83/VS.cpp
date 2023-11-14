571f19701f602c3c3cc78eb210d071e9ac76a1ac
CornCobby <142827968+CornCobby@users.noreply.github.com>
2023-11-14 00:55:35 +0000

    Mon, Nov 13, 2023, 4:55 PM -08:00

--- CS112/Assignments/Asign05/CardPack.cpp
+++ CS112/Assignments/Asign05/CardPack.cpp
@@ -88,7 +88,7 @@ using namespace std;
 
     void CardPack::addCard(const GameCard& newCard) {
     if (packSize < MAX_PACKSIZE) {
-        thePack[packSize] = new GameCard(newCard);
+        thePack[packSize] = GameCard card(newCard);
         packSize++;
     }
     else {
