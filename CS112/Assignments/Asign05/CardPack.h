ec14066e7ea7989c05a69f39c9f63363a602f6fc
CornCobb83 <143015909+CornCobb83@users.noreply.github.com>
2023-11-27 20:13:39 +0000

    Mon, Nov 27, 2023, 12:13 PM -08:00

--- CS112/Assignments/Asign05/CardPack.h
+++ CS112/Assignments/Asign05/CardPack.h
@@ -31,7 +31,7 @@ public:
     // Declarations for mutators
     void setName(string name);
     void setSize(int size);
-    void setCard(const RareCard& newCard);
+    void setCard(int index, const RareCard& newCard);
 
     // Declarations for other methods
     CardPack& operator=(const CardPack& other);
