fc76885a629f75e2cf7e196c88b3c1b81fba4b06
CornCobb83 <143015909+CornCobb83@users.noreply.github.com>
2023-11-29 19:07:58 +0000

    Wed, Nov 29, 2023, 11:07 AM -08:00

--- CS112/Assignments/Asign05/RareCard.h
+++ CS112/Assignments/Asign05/RareCard.h
@@ -35,7 +35,7 @@ public:
 
     // Declarations for other methods
     bool operator ==(const RareCard& rhs) const;
-    RareCard& operator =(const RareCard& rhs) const;
+    RareCard& operator =(const RareCard& rhs);
     string to_string() const;
     void display() const;
 
