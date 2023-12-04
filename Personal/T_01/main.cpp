c794c524ee179bf5996deb77dfdfd590b16cc000
CornCobb83 <143015909+CornCobb83@users.noreply.github.com>
2023-12-04 21:42:22 +0000

    Mon, Dec 4, 2023, 1:42 PM -08:00

--- Personal/T_01/main.cpp
+++ Personal/T_01/main.cpp
@@ -1,40 +1,34 @@
 #include <iostream>
-#include <stdexcept>
-
 using namespace std;
 
-// Function to clear the input buffer
-void clearInputBuffer() {
-    cin.clear();
-    while (cin.get() != '\n') {
-        continue;
-    }
+void FindNumber(int number, int lowVal, int highVal) {
+   int midVal;
+
+   midVal = (highVal + lowVal) / 2;
+   cout << number;
+   cout << " ";
+   cout << midVal;
+
+   if (number == midVal) {
+      cout << " p" << endl;
+   }
+   else {
+      if (number < midVal) {
+         cout << " q" << endl;
+         FindNumber(number, lowVal, midVal);
+      }
+      else {
+         cout << " r" << endl;
+         FindNumber(number, midVal + 1, highVal);
+      }
+   }
 }
 
 int main() {
-    int userNum;
-    int divNum;
+   int number;
 
-    cin.exceptions(ios::failbit);  // Allow cin to throw exceptions
+   cin >> number;
+   FindNumber(number, 0, 12);
 
-    try {
-        if (!(cin >> userNum)) {
-            throw runtime_error("Input Exception: basic_ios::clear: iostream error");
-        }
-
-        if (!(cin >> divNum)) {
-            throw runtime_error("Input Exception: basic_ios::clear: iostream error");
-        }
-        else if (divNum == 0) {
-            throw runtime_error("Runtime Exception: Divide by zero!");
-        }
-        else {
-            cout << userNum / divNum << endl;
-        }
-    }
-    catch (const runtime_error& e) {
-        cout << e.what() << endl;
-    }
-
-    return 0;
+   return 0;
 }
